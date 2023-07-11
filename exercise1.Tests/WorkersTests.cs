0469ad9f074f892e4eeddda5fc06174e5caa75ed
Michał Serda <michal.serda@billennium.com>
2023-07-11 14:17:29 +0200

    refactoring

--- exercise1.Tests/WorkersTests.cs
+++ exercise1.Tests/WorkersTests.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Interface;
 using Application.Services;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace exercise1.Tests
 {
